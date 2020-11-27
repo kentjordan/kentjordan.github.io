4a3fd1a308fe39542e1f128f25dacfa14d340570
Kent Jordan <kentaki.dev@gmail.com>
2020-11-27 12:49:55 +0800

    Added welcome screen

--- js/app.js
+++ js/app.js
@@ -4,10 +4,33 @@ const mNavHambergurIcon = document.querySelector('.mobile-nav-bar-hamburger-icon
 const mNavItemContainer = document.querySelector('.mobile-nav-item-menu-container');
 const mNavItemCloseBtn = document.querySelector('.nav-item-close');
 
+var bgAudio = document.querySelector('#bgAudio');
+
 function main(){
 
     mNavItemContainer.style.visibility = "hidden";
     MobileNavHamburgerButton();    
+
+    var welcome = document.querySelector('.welcome'); 
+
+    window.onload = () => {
+
+        welcome.addEventListener('click', ()=>{
+
+            welcome.style.opacity = '0';
+            
+            document.documentElement.style.overflow ='visible';
+
+            if(document.getElementById('typed-config-js') <= 0){
+                const typedJS = document.createElement('script');
+                typedJS.id = 'typed-config-js';
+                typedJS.src = "./js/typed-config.js";
+                document.body.appendChild(typedJS);
+            }
+
+            bgAudio.play();
+        });
+    }
 
 }
 
